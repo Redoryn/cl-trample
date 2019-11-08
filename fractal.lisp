0de2102d5448e6dddd743705873e29b939ee3663
Alex Stults <redoryn@gmail.com>
2019-11-07 21:16:42 -0500

    Optimized fractal generation time.

--- fractal.lisp
+++ fractal.lisp
@@ -28,8 +28,8 @@
 
 (defun escape-time (px py max-depth)
   (declare (optimize (speed 3) (safety 0)))
-  (declare (type integer px py iteration max-depth))
-  (declare (type double-float x0 y0 x y xtemp))
+  (declare (type fixnum px py max-depth))
+  
   
   (let ((x0 (x->fractal-x px))
 	(y0 (y->fractal-y py))
@@ -37,14 +37,16 @@
 	(y 0.0)
 	(xtemp 0.0)
 	(iteration 0))
+    (declare (type double-float x0 y0 x y xtemp))
+    (declare (type fixnum iteration))
     (loop
-       do (progn
-	    (setf xtemp (+ (- (* x x)
-			      (* y y))
-			   x0))
-	    (setf y (+ (* 2 x y) y0))
-	    (setf x xtemp)
-	    (setf iteration (+ 1 iteration)))
+       do
+	 (setf xtemp (+ (- (* x x)
+			   (* y y))
+			x0))
+	 (setf y (+ (* 2 x y) y0))
+	 (setf x xtemp)
+	 (setf iteration (+ 1 iteration))
        until (or (>= (+ (* x x) (* y y)) 4.0)
 		 (>= iteration max-depth))
 	 )
@@ -59,19 +61,23 @@
   (setf *fractal-width* width)
   (setf *fractal-height* height)
   (setf *do-refresh* t)
-  (when (= 0 (mod *zoom-level* 5))
+  (when (= 0 (mod *zoom-level* 2))
     (setf *max-depth* (ceiling (* 1.25 *max-depth*))))
   )
 
 
 (defun update (world)
   (declare (optimize (speed 3) (safety 0)))
-  (declare (type integer i count))
-  (declare (type system-area-pointer tex-ptr))
+  (declare (type fixnum *width* *height*))
+  
+  (declare (ignore world))
   (when *do-refresh*
     (let ((tex-ptr (sdl2:lock-texture *fractal-texture*))
 	  (count (* *width* *height*))
 	  (max-depth *max-depth*))
+      (declare (type SB-SYS:system-area-pointer tex-ptr))
+      (declare (type fixnum count))
+      (print (type-of tex-ptr))
       (dotimes (i count)
 	(multiple-value-bind (px py) (index->coord i *width*)
 	  (setf (cffi:mem-aref tex-ptr :unsigned-int i)
@@ -87,7 +93,7 @@
     (setf *do-refresh* nil)))
 
 (defun on-mouse-click (px py state)
-  
+  (declare (ignore state))
   (let ((fx (x->fractal-x px))
 	(fy (y->fractal-y py))
 	(f-width (* 0.35 *fractal-width*))
@@ -102,7 +108,9 @@
     )
   )
 
-(defun draw (world))
+(defun draw (world)
+  (declare (ignore world))
+  )
 
 (start #'setup #'update #'draw)
 
